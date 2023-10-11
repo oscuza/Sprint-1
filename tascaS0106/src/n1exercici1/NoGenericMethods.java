ac3090c737529eb767f0dcb1090eb3a9199d3b58
Oscar Cuevas <oscuza@gmail.com>
2023-10-11 18:32:59 +0200

    tascaS0106 exercici 1 final trec l'enunciat

--- tascaS0106/src/n1exercici1/NoGenericMethods.java
+++ tascaS0106/src/n1exercici1/NoGenericMethods.java
@@ -1,13 +1,6 @@
 package n1exercici1;
 
-/**
- *Crea una classe anomenada NoGenericMethods que emmagatzemi tres objectes del 
- * mateix tipus, juntament amb els mètodes per a emmagatzemar i extreure 
- * aquests objectes i un constructor per a inicialitzar els tres.
- * Comprova que els arguments es poden posar en qualsevol posició
- * en la crida al constructor.
- * @author OCuevas
- */
+
 public class NoGenericMethods<T> {
 
     private T obj1;
